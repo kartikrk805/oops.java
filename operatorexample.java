db685177acf531159d4979f6cbeab16ed8693e60
Kartik R K <kartikrk805@gmail.com>
2026-09-08 08:04:01 +0530

    Add files via upload

---
+++ operatorexample.java
@@ -0,0 +1,11 @@
+public class operatorexample{
+    public state void main(string args[]){
+        int a=10;
+        int b=5;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
